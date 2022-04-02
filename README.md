a42b2fd84ab46498e9c63841f997c34f35cf56ab
Philippe <p.vallerand@bell.net>
2022-04-02 12:28:35 -0400

    Spring initial build

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TP_Transactionnel_Spring
+
 TP avec Spring
---
+++ src/main/java/com/example/tp_transactionnel_spring/TpTransactionnelSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp_transactionnel_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpTransactionnelSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpTransactionnelSpringApplication.class, args);
+    }
+
+}
