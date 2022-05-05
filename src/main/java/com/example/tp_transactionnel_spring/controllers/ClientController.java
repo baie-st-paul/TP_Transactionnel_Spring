8e5e2e3e04a3b6ada1a609c24683d45578d6b9ef
Philippe <p.vallerand@bell.net>
2022-05-05 18:53:48 -0400

    created function for get all clients request on server

--- src/main/java/com/example/tp_transactionnel_spring/controllers/ClientController.java
+++ src/main/java/com/example/tp_transactionnel_spring/controllers/ClientController.java
@@ -1,5 +1,31 @@
 package com.example.tp_transactionnel_spring.controllers;
 
+import com.example.tp_transactionnel_spring.DTO.DTOModels.ClientDTO;
+import com.example.tp_transactionnel_spring.DTO.Mapper;
+import com.example.tp_transactionnel_spring.service.ServiceLibrary;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@RestController
+@RequestMapping("/clients")
 public class ClientController {
 
+    private final ServiceLibrary serviceLibrary;
+    private final Mapper mapper;
+    public ClientController(ServiceLibrary serviceLibrary) {
+        this.serviceLibrary = serviceLibrary;
+        this.mapper = new Mapper();
+    }
+
+    @GetMapping
+    @CrossOrigin(origins = "http://localhost:3000")
+    public List<ClientDTO> getAllClients(){
+        return serviceLibrary.findAllClient().stream().map(mapper::clientToDTO).collect(toList());
+    }
 }
